11e151867613a29cdfa187d67409f142d80cb827
stavo343 <nintendoplayr13@aol.com>
2017-03-18 17:59:08 -0400

    tweaking ip get

--- server.js
+++ server.js
@@ -1,18 +1,29 @@
 var express = require('express');
-var port = process.env.PORT;
+var port = process.env.PORT || 1337;
 
 app = express();
 
 var parsed = {"ipaddress":"_", "language":"_", "software":"_"};
 
 app.get('/', function(req, res) {
-  var ip = JSON.stringify(req.headers.host).toString();
-  parsed.ipaddress = ip.slice(1, ip.length-6);
+  var ip = JSON.stringify(req.connection.remoteAddress).toString();
+
+  if (ip.indexOf(':') != -1) {
+    parsed.ipaddress = ip.slice(ip.lastIndexOf(':')+1, ip.length-1);
+  } else {
+    parsed.ipaddress = ip.slice(1, ip.length-1);
+  }
+
   var lang = JSON.stringify(req.headers["accept-language"].toString());
+
   parsed.language = lang.slice(1, lang.indexOf(','));
+
   var soft = JSON.stringify(req.headers["user-agent"].toString());
+
   parsed.software = soft.slice(soft.indexOf('(') + 1, soft.indexOf(')'));
+
   console.log(req.headers);
+
   res.send(JSON.stringify(parsed));
 });
 
